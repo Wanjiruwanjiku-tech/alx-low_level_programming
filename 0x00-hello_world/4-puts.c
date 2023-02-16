81d93cefd48cea08d8f5b356b76296c6c3c3c29a
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-02-16 10:38:27 -0800

    C programming language basics

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+ * Main is the Entry point
+ * 
+ * Return is zero by default
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0)
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,2 @@
+Getting Started With C programming language.
+Hello, World!
