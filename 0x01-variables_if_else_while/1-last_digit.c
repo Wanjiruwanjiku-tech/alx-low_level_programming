5e183975f7dccf93210542ab814c96bc06c2e39f
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-02-20 18:04:53 -0800

    Final

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
+/**
+* main -  prints random nunbers and show if greater
+* than 5, lesser than 6 or 0
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	else if (m == 0)
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	else
+		printf("Last digit if %d is %d and is less than 6 and not 0\n", n, m);
+
+	return (0);
+}
