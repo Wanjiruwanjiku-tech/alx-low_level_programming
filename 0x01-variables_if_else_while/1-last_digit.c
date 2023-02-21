db734d331ad217ba7172f6c30ad4e6f1a4f820c1
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-02-20 18:08:34 -0800

    OF

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,8 @@ int main(void)
 	else if (m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	else
-		printf("Last digit if %d is %d and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+
 
 	return (0);
 }
