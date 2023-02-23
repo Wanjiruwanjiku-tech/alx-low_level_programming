7987e154a56edbd1bc91ae1f2aec77fc5a21d9ba
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-02-23 05:11:41 -0800

    Task Five

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*more_numbers - A function that prints ten times
+*the numbers from 0 to 14
+*
+*Return: 0-14 x10 followed by \n
+*/
+void more_numbers(void)
+{
+	int i, ro;
+
+	for (ro = 0; ro < 10; ro++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
