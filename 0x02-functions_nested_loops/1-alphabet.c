9d24e16c547c0d0532fe338882dac041ee188933
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-02-22 18:03:50 -0800

    Correction for task one

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,15 +7,12 @@
 */
 void print_alphabet(void)
 {
-	char c;
+	char c = 'a';
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (c <= 'z')
 	{
 		_putchar(c);
+		c++;
 	}
-
 	_putchar('\n');
-
-
-	return ();
 }
