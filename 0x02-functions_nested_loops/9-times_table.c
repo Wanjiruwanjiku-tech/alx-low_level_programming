bcbfa14f6fe78a0b76188d7fd5fcea13c0bb7e54
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-02-22 17:13:09 -0800

    Correction for Task Nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,37 +8,36 @@
 */
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int x = 0;/*factor*/
+	int y;/* count */
+	int x;/* computed value */
 
-	for (x = 0; x <= 9; x++)
+	while (x < 10)
 	{
-		for (y = 0; y <= 9; y++)
+		y = 0;
+		while (y < 10)
 		{
 			z = x * y;
 
 			if (z > 9)
 			{
-				u = z % 10;
-				d = (z - u) / 10;
-
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
 			}
-			else
+			else if (y != 0)
 			{
-				if (y != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-
-				-putchar(z + '0');
+				_putchar(' ');
+				_putchar(z + '0');
 			}
-		}
 
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++
+		}
 		_putchar('\n');
+		x++
 	}
 }
