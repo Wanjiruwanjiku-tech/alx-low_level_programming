650bbc41f326c224eef526a703d1d80a5391b742
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-03-10 09:00:45 -0800

    Learning about Static libraries in C

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_puts - Prints string
+*@str: The string
+*Return: void
+*/
+void _puts(char *str)
+{
+
+}
+
+#include<stdio.h>
+
+/**
+*main - The Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
