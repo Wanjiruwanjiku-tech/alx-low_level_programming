ea228da128af299233d5b2d859e91e698b498b7d
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-03-16 05:59:28 -0700

    Allocate Memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*malloc_checked - A Function that dynamically allocates
+*memory using malloc function
+*@b: The size to assign
+*
+*Return: A Pointer to allocated memory or a status
+*value of 98 if failure
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *pointer = malloc(b);
+
+	if (pointer == NULL)
+	{
+		exit(98);
+	}
+	return (pointer);
+}
