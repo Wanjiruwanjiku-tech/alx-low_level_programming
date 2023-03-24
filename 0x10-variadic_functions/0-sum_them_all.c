00555af44bb766e0cf65c5a31ebd928915c7975c
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-03-24 07:44:05 -0700

    Variadic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,21 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
-*sum_them_all - A Function that sums up all its
-*parameters
-*@n: The fixed psrsmeter
-*@...: The variable number of parameters to sum up
-*
-*Return: The sum of all parameters
-*/
+ * sum_them_all - return the sum of all its parameters
+ * @n: number of extra params
+ * Return: total sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum_them_all(const unsigned int n, ...)
-{
-	va_list nums;
-	unsigned int index, sum = 0;
+	unsigned int i;
+	va_list list;
+	int sum;
 
-	va_start(nums, n);
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-
-	va_end(nums);
+	va_end(list);
 
 	return (sum);
 }
