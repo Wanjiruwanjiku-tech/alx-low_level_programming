3518db436dce78088e550535f6fe13107473c4b5
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-03-30 04:48:30 -0700

    Returns the number of elements in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* list_len - returns the number of elements 
+* list_len - returns the number of elements
 * in a linked list_t list
 * @h: the linked list to check
 * Return: number of elements in @h
