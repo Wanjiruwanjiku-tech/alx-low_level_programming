92f09669d55fafe70bffa79bef50cb017f37f324
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-06-09 18:49:57 -0700

    Hash Maps

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
 *@key: Pointer to the key
 *@size: The size of the array
 *
-*Return: Returns the index at which the 
+*Return: Returns the index at which the
 *	key/value pair should be stored in the array of the hash table
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
