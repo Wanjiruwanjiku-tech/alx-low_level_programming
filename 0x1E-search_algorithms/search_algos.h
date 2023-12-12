fcddb3821b737022cf63c4319d462d8a9a62d2b2
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-11 23:38:19 -0800

    My Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,14 @@
-#ifdef SEARCH_ALOGS_H
+#ifndef SEARCH_ALOGS_H
 #define SEARCH_ALOGS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /* My Project Prototypes */
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_ALOGS_H */
